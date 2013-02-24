5f799ac47071cd5b284ea29d7d9826509f36b7b8
Ruud van Falier <ruud@dottech.nl>
2013-02-24 14:24:02 +0100

    Almost finished...

---
+++ include/js/send-mail.js
@@ -0,0 +1,49 @@
+
+$(function() {
+	
+	var contactForm = $('#contact-form');
+	
+	contactForm.find("a.close").click(function () {
+	    contactForm.find("fieldset.confirmation").hide();
+	});
+
+	contactForm.submit(function()
+	{
+		if(contactForm.valid())
+		{
+			contactForm.find('.submit-area').addClass('loading');
+			var formValues = $(this).serialize();
+			
+			$.post($(this).attr('action'), formValues, function (data) {
+				setTimeout(function () { 
+					hideLoader();
+					contactForm.find('.submit-area').removeClass('loading').addClass("success"); 
+					contactForm.clearForm();
+
+					contactForm.find("fieldset.confirmation").show();
+
+					setTimeout(function () {
+					    contactForm.find(".submit-area .icon").hide();
+					}, 1000);
+				}, 1000);
+			});
+		}
+		return false
+	});
+		
+});
+
+
+$.fn.clearForm = function() {
+  return this.each(function() {
+    var type = this.type, tag = this.tagName.toLowerCase();
+    if (tag == 'form')
+      return $(':input',this).clearForm();
+    if (type == 'text' || type == 'password' || tag == 'textarea')
+      this.value = '';
+    else if (type == 'checkbox' || type == 'radio')
+      this.checked = false;
+    else if (tag == 'select')
+      this.selectedIndex = -1;
+  });
+};
---
+++ sendmail.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" %>
+<script runat="server">
+    public void Page_Load(object sender, EventArgs e)
+    {
+        string name = Request.Form["name"];
+        string email = Request.Form["email"];
+        string message = Request.Form["message"];
+        string redirect = Request.Form["redirect"];
+        
+        if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(message))
+        {
+            System.Net.Mail.MailMessage m = new System.Net.Mail.MailMessage();
+            
+            m.From = new System.Net.Mail.MailAddress("info@dottech.nl");
+            m.To.Add(new System.Net.Mail.MailAddress("ruud@dottech.nl"));
+            m.Subject = "Contactformulier dottech.nl";
+            m.Body = message;
+
+            System.Net.Mail.SmtpClient c = new System.Net.Mail.SmtpClient();
+            // c.Send(m);
+        }
+ }
+</script>
